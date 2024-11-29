6fed2d9a30fc36aa91a8262881f35d65bbe8be2a
Saeed Heydari <saeedhpro@gmail.com>
2024-11-29 19:25:54 +0330

    change: change logo

--- components/loading/PageLoading.vue
+++ components/loading/PageLoading.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="splash_page fixed top-0 bottom-0 left-0 right-0 z-[100000000] flex flex-col justify-center items-center">
     <img src="@/assets/images/logo.png" alt="Beauty Map"/>
-    <sync-loader class="mt-8" :loading="true" :color="'#fff'" :size="'20px'" margin="10px"/>
+    <sync-loader class="mt-8" :loading="true" :color="'#133C3E'" :size="'20px'" margin="10px"/>
   </div>
 </template>
 
